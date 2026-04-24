6e532899caa5191296122cd35c061a7f91f7dc0e
Juan Carlos <jkarloslc@gmail.com>
2026-04-23 23:31:55 -0600

    fix: agregar layout.tsx a módulo Hospitality (sidebar)

---
+++ app/hospitality/layout.tsx
@@ -0,0 +1,5 @@
+import DashLayout from '@/components/layout/DashLayout'
+
+export default function HospitalityLayout({ children }: { children: React.ReactNode }) {
+  return <DashLayout>{children}</DashLayout>
+}
